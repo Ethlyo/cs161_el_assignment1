456456848234612b08c189eb07649a6a512f3200
Ethlyo <77252686+Ethlyo@users.noreply.github.com>
2021-01-10 17:40:20 -0800

    Add files via upload

---
+++ cs161_el_assignmentone/src/edu/cgcc/cs161/assignment1q2.java
@@ -0,0 +1,21 @@
+package edu.cgcc.cs161;
+//HEADER
+//Program Name: Assignment1
+//Author: Ethan Lyons
+//Class: CS2161 Winter 2021
+//Date: 01/10/2021
+//Description: Prints "Hello World!"
+
+/*Psuedocode
+ * Program start
+ * print "Hello World"
+ * Program end
+*/
+public class assignment1q2 {
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+	}
+}
+/* Footer
+*Hello World!
+*/
